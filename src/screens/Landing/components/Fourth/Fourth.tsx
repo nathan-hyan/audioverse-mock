aeafc7bcce0652f54d7d8c1d006850a1bc6366e1
Exequiel Mleziva <exequiel@hyan.dev>
2024-08-22 10:36:33 -0300

    feat: :construction: wip updated deps and working on fourth blade

--- src/screens/Landing/components/Fourth/Fourth.tsx
+++ src/screens/Landing/components/Fourth/Fourth.tsx
@@ -30,10 +30,13 @@ function Fourth() {
         </p>
       </div>
 
+      <div className={styles.verticalLine} />
+
       <div className={styles.rightSide}>
         <img
           src='https://plus.unsplash.com/premium_photo-1663047632991-a2de16748c44?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           alt='rightSide'
+          className={styles.img}
         />
       </div>
     </div>
